b66247fb0a7bc2e54258f47c8445630ce6524f06
tianik909 <anik909@hotmail.com>
2016-06-22 18:34:16 +0600

    learning data types

--- fuxam/src/DataTypes.java
+++ fuxam/src/DataTypes.java
@@ -6,11 +6,22 @@
  */
 public class DataTypes {
     public static void main(String[] args) {
+
+
+
         //int stands for integer
         int NormalNumber = 9;
 
         System.out.println(NormalNumber);
         //this prints out the value of NormalNumber
+
+        float FloatNumber = 9.1f;
+        System.out.println(FloatNumber);
+        /*this prints out the value of FloatNumber
+        *NOTE : I added 'f' after the float number
+        *REASON : java assumes float to be double by default
+        * so to override it , I added the 'f' at the end
+        */
 
     }
 }
